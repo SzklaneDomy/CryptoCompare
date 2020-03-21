8dccfd06b82641d17c4e9a3523bdd819830763f0
unknown <zlotopolscy2@gmail.com>
2020-03-21 20:28:40 +0000

    Updates

---
+++ precache-manifest.ab1de5384e3b2b9f2f66ab4839ed8244.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a39fc9e5e95392f3693a7e9f4845ccd",
+    "url": "/CryptoIndex/index.html"
+  },
+  {
+    "revision": "19db1bae9eef4753f38c",
+    "url": "/CryptoIndex/static/css/main.a9a2a0a2.chunk.css"
+  },
+  {
+    "revision": "d44577822c13e96e0e09",
+    "url": "/CryptoIndex/static/js/2.62e56a24.chunk.js"
+  },
+  {
+    "revision": "bc1606c6db11c396205f31e2fe0b9d0c",
+    "url": "/CryptoIndex/static/js/2.62e56a24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19db1bae9eef4753f38c",
+    "url": "/CryptoIndex/static/js/main.a805a20c.chunk.js"
+  },
+  {
+    "revision": "0b82b33610e226313e79",
+    "url": "/CryptoIndex/static/js/runtime-main.8995ea5c.js"
+  }
+]);
